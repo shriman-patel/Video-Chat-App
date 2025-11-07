01c68bf8c943e3078aa88b565f592d238a59c322
shriman patel <shrimanp304@gmail.com>
2025-11-07 11:17:01 +0530

    Update environment.js

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,6 +1,6 @@
 let IS_PROD = true;
 const server = IS_PROD ?
-    "https://apnacollegebackend.onrender.com" :
+    "video-chat-app-production-0407.up.railway.app" :
 
     "http://localhost:8000"
 
